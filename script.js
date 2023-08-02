32af6ca5decd0bac2f85100b95ee685f6614ffcf
Kae <139962400+kaedeluxe@users.noreply.github.com>
2023-08-02 10:28:44 +0100

    revert incorrect script.js, add cpu random choice
    uploaded the wrong script.js, fixed to the correct one

--- script.js
+++ script.js
@@ -1,55 +1,28 @@
-
-/* Function to add 7 to a given value */
-
-function add7(inputNo) {
-  return Number(inputNo.value) + 7;
-}
-function showAdd7() {
-let userNo = document.getElementById("add7input");
-document.getElementById("add7result").innerHTML = add7(userNo);
-}
-
-/* Function to multiply 2 user given values */
-
-function multiply(a, b) {
-  return Number(a.value) * Number(b.value);
-}
-function showMultiply() {
-  let inputA = document.getElementById("multiplyFirstNo"),
-  inputB = document.getElementById("multiplySecondNo");
-  document.getElementById("multiplyResult").innerHTML = multiply(inputA, inputB);
+/*
+  Function to get a random choice from rock/paper/scissors
+*/
+function getCpuChoice() {
+  let CpuChoice,
+    randNo = Math.random();
+  if (randNo < 0.34) {
+    CpuChoice = "Rock";
+  } else if (randNo >= 0.34 && randNo < 0.67) {
+    CpuChoice = "Paper";
+  } else {
+    CpuChoice = "Scissors";
+  }
+  return CpuChoice;
 }
 
-/* Function to capitalise only the first letter of a given string */
 
-function capitalise(userString) {
-  let firstLetter = userString.value.substr(0,1),
-  restString = userString.value.substr(1),
-  outString;
-  firstLetter = firstLetter.toUpperCase();
-  restString = restString.toLowerCase();
-  outString = firstLetter.concat(restString);
-  return outString;
-}
-function showCap() {
-  let inputString = document.getElementById("inputCap");
-  document.getElementById("outputCap").innerHTML = capitalise(inputString);
-}
 
-/* Function to get last letter of a string */
 
-function getLast(userString) {
-  let strLength = userString.value.length,
-  lastLetter = userString.value.substr(strLength - 1);
-  return lastLetter;
-}
-function showLast() {
-  let inputString = document.getElementById("inputLast");
-  document.getElementById("outputLast").innerHTML = getLast(inputString);
-}
 
-/* Function to clear strings */
 
-function clearResult(elementID) {
-  document.getElementById(elementID).innerHTML = "N/A";
+/*
+  Function for testing purposes
+  Without a console.log() we have to use the Window Alert method
+*/
+function testFunc() {
+  alert(getCpuChoice());
 }
