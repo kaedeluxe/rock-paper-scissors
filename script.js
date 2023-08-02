a9951bde3a2488a38a7d6e18069f509bc1d6867f
Kae <139962400+kaedeluxe@users.noreply.github.com>
2023-08-02 15:48:16 +0100

    rework playRound function, adjust html
    The 'playRound()' function now starts based off of a play button and captures the user's input with a prompt that can then be compared to the cpu.
    Currently the 'switch (playerInput)' to give the user shortcuts for rock paper scissors, r, p, s, is broken and not adjusting the playerChoice variable to the correct string.

--- script.js
+++ script.js
@@ -14,23 +14,53 @@ function getCpuChoice() {
   return cpuChoice;
 }
 
-function playRound(playerChoice) {
-  let cpuChoice = getCpuChoice(),
-    evalChoices = playerChoice + cpuChoice;
+let countWin,
+  countLose,
+  countDraw
+  i;
 
+function playRound() {
+  let cpuChoice = getCpuChoice(),
+    playerInput = prompt("Enter rock, paper or scissors:"),
+    playerChoice,
+    evalChoices,
+    result;
+  // playerChoice = playerInput.toLowerCase();
+  switch (playerInput) {
+    case "r":
+    case "R":
+      playerChoice = "rock";
+    case "p":
+    case "P":
+      playerChoice = "paper";
+    case "s":
+    case "S":
+      playerChoice = "scissors";
+    default:
+      playerChoice = playerInput.toLowerCase();
+  }
+  evalChoices = playerChoice + cpuChoice;
   switch (evalChoices) {
     case "rockrock":
     case "paperpaper":
     case "scissorsscissors":
+      ++countDraw;
       alert(`Draw! You both chose ${playerChoice}.`);
       break;
     case "rockscissors":
     case "paperrock":
     case "scissorspaper":
+      ++countWin;
       alert(`You win! ${playerChoice} beats ${cpuChoice}.`);
       break;
+    case "rockpaper":
+    case "paperscissors":
+    case "scissorsrock":
+      ++countLose;
+      alert(`You lose! ${cpuChoice} beats ${playerChoice}.`);
+      break;
     default:
-        alert(`You lose! ${cpuChoice} beats ${playerChoice}.`)
+      alert(`${playerChoice} is not a valid argument, please try again.`)
       break;
   }
 }
